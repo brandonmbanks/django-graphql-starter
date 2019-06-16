c03f7be5d7beb85a984e5e32efcb262b82d399b8
Brandon Banks <b93banks@gmail.com>
2019-06-16 07:16:09 -0400

    add graphql routes and sample query

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "graphene_django",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+GRAPHENE = {"SCHEMA": "mysite.schema.schema"}  # Where your Graphene schema lives
