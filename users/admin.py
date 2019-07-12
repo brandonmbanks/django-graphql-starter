53abbf8210517c473e77a8d29914b54828d53d9d
Brandon Banks <b93banks@gmail.com>
2019-07-11 22:13:07 -0400

    remove customer forms and tests file

--- users/admin.py
+++ users/admin.py
@@ -2,12 +2,9 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Permission
 from users.models import User
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 
 
 class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
     model = User
     list_display = ["email", "username"]
 
